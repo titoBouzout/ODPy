3335ae8d3dad8916d567b31edde30b74e4318a6c
Tito Bouzout <tito@ampcoders.com>
2010-08-31 16:02:33 -0300

    First semifunctional working version.

--- ODPy.js
+++ ODPy.js
@@ -1,18 +1,62 @@
-/*
+﻿/*
 	Main javascript object..
 */
 
-var ODPy = {
-	
-	/* loads the starting inteface..*/
-	load : function()
+ODP = {	
+
+	e : '…', /* ellipsis shortcut */
+
+	load : function()// loads the starting inteface..
 	{
-		/* loading category box */
-		
-			this.categoryFocused = $('#category');
+		//first the status bar
+		this.status = $('.status');
+		this.statusSet('loading document'+this.e);
+		this.statusHide();
+
+		//now load basis elements
+		this.loadElements();
+
+		//display document loaded
+		this.statusSet('document loaded'+this.e);
+		this.statusHide();
+	},
+	loadElements : function()// loads basic inteface elements
+	{
+	},
+	loadCategory : function(aCategory)// loads a new category
+	{
+		if(aCategory != this.loadedCategory)
+		{
+			this.loadedCategory = aCategory;
 			
-			// adding an initial value to the category box ( the initial value comes from title attribute loaded by jquery.inputDefault.js )
-			
-			this.categoryFocused.inputDefault();
+			this.statusSet('loading category '+aCategory+this.e);
+			this.statusHide();
+		}
+	},
+	statusSet : function(aString)//display information in the status bar
+	{
+		//manages the show and hide of the status, avoids to many flashing.
+		if(!this.statusElementsShowing)
+		{
+			this.statusElementsShowing = 0;
+			this.statusElementsShown = 0;
+		}
+		this.statusElementsShowing++;
+
+		//show the status bar if it is hidden
+			if(this.status.is(':hidden'))
+				this.status.fadeIn();
+		//set the status
+		this.status.html(aString);
+	},
+	statusHide : function()//hide the status bar if no more element are in queue
+	{
+		setTimeout(function(){
+								ODPy.statusElementsShown++; 
+								if(ODPy.statusElementsShown == ODPy.statusElementsShowing)
+									ODPy.status.fadeOut();
+							}, 1300);
 	}
+	
+	,
 };
--- readme.txt
+++ readme.txt
@@ -1,9 +1,17 @@
-This holds an experimental jQuery interface for dmoz editing.
+This holds an experimental HTML5 interface for dmoz navigation and editing powered by many JavaScript libraries.
+
+Currenlty non functional (work in progress).
+
+Steps of this project:
+
+	1 - Write semantically correct ODP documents ( ODP public pages on HTML5 ).
+	2 - Write core functions to parse data and dinamically build such documents. ( near future will get the data from dmoz APIs )
+	3 - Make use of drag and drop and many other functions provided by the HTML5 expecification and/or other power javascipt libraries for editing task
 
 Source Code at:
 
-	git@github.com:titoBouzout/ODPy.git
+	http://github.com/titoBouzout/ODPy
 
 URL:
 
-	http://www.dmoz.org/desc/Bookmarks/D/development
+	http://www.dmoz.org/desc/Test/World_Test/World/Espa%C3%B1ol/Editores/development
