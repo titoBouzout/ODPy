1b0c40791388f9c39d135edc56579ffa72e2b3b4
Tito Bouzout <tito@ampcoders.com>
2010-09-01 09:14:49 -0300

    Testing capture SWFAddress.

--- ODPy.js
+++ ODPy.js
@@ -28,7 +28,7 @@ ODP = {
 		//$("#category").inputDefault();
 		//$("#search").inputDefault();
 		
-		SWFAddress.addEventListener(SWFAddressEvent.CHANGE, function(event){ ODPy.onLocationChange(event);});
+		SWFAddress.addEventListener(SWFAddressEvent.CHANGE, function(event){ ODPy.onLocationChange(event);}, true);
 	},
 	loadCategory : function(aCategory)// checks for loading a new category
 	{
@@ -94,7 +94,6 @@ ODP = {
 			this.setTitle(aCategory);
 			parseCategory(categoryGetFromURL(aCategory), function(aCategory, aData){ ODPy.browse(aCategory, aData);});
 		}
-		
 	},
 	browse : function(aCategory, aData)
 	{
